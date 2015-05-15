a1fd8ace853be7b391153491f627a7a399e20b72
tvart <vartan.torossian@gmail.com>
2015-05-15 19:56:33 +0200

    first push

---
+++ src/Formater.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tvart\Facebook;
+
+class Formater
+{
+    public static function idx(array $array, $key, $default = null) {
+        return array_key_exists($key, $array) ? $array[$key] : $default;
+    }
+
+    public static function he($str) {
+        return htmlentities($str, ENT_QUOTES, "UTF-8");
+    }
+}
